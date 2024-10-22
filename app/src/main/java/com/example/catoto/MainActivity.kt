d174491621012f2ca475b9a1985592cecf941ba8
pmatatias <matatias97@gmail.com>
2024-10-23 01:57:01 +0700

    learn Api

--- app/src/main/java/com/example/catoto/MainActivity.kt
+++ app/src/main/java/com/example/catoto/MainActivity.kt
@@ -1,20 +1,70 @@
 package com.example.catoto
 
 import android.os.Bundle
+import android.provider.Settings.Global
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.catoto.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+//        enableEdgeToEdge()
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        getQuote()
+        binding.nextBtn.setOnClickListener{
+            getQuote()
+        }
+    }
+
+    private fun getQuote(){
+        setInProgress(true)
+        GlobalScope.launch {
+            try {
+                val response = RetrofitInstance.quoteApi.getRandomQuote()
+                runOnUiThread{
+                    setInProgress(false)
+                    response.body()?.first()?.let {
+                        setUi(it)
+                    }
+                }
+            }catch (e: Exception){
+                runOnUiThread{
+                    setInProgress(false)
+                    Toast.makeText(applicationContext, "Someting went wrong",Toast.LENGTH_SHORT).show()
+                }
+
+            }
+        }
+
+    }
+
+    private fun  setUi (quote: QuoteModel){
+        binding.etText.text = quote.q
+        binding.etSts.text= quote.a
+    }
+
+    private  fun  setInProgress (inProgress: Boolean){
+        if(inProgress){
+            binding.progressBar.visibility = View.VISIBLE
+            binding.nextBtn.visibility = View.GONE
+        }else{
+            binding.progressBar.visibility = View.GONE
+            binding.nextBtn.visibility = View.VISIBLE
         }
     }
 }
---
+++ app/src/main/java/com/example/catoto/QuoteModel.kt
@@ -0,0 +1,6 @@
+package com.example.catoto
+
+data class QuoteModel(
+    val q : String,
+    val a : String
+)
